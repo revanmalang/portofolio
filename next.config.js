99b668afb772b580206bdc70cdfd5a87a7ff0149
revanmalang <revanmalang584@gmail.com>
2025-05-27 09:21:25 +0700

    Fix workflow deploy GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+  output: 'export',   // penting untuk static export
+  images: {
+    unoptimized: true,  // karena image optimization Next.js tidak bekerja dengan static export
+  },
+}
 
 module.exports = nextConfig;
